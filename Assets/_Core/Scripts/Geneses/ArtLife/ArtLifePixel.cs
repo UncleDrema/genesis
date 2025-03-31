3ae0cea80cce8568a49926d157f4c8b889e50e04
Andrey Kuzin <141935903+KuzinAndrey0@users.noreply.github.com>
2025-03-31 23:14:12 +0300

    feat: cell class

--- Assets/_Core/Scripts/Geneses/ArtLife/ArtLifePixel.cs
+++ Assets/_Core/Scripts/Geneses/ArtLife/ArtLifePixel.cs
@@ -1,5 +1,6 @@
-﻿using Genesis.GameWorld;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Genesis.GameWorld;
 
 namespace Geneses.ArtLife
 {
@@ -7,5 +8,31 @@ namespace Geneses.ArtLife
     {
         public bool IsDirty { get; set; }
         public Color32 Color => UnityEngine.Color.black;
+        
+        // Параметры клетки
+        public byte[] Genome { get; private set; } = new byte[64];
+        public int CommandCounter { get; private set; }
+        public int ActiveGeneIndex { get; private set; } // индекс текущей активной ячейки генома
+        public int Direction { get; private set; } // направление от 0 (вправо) до 7 (право-вниз)
+        public Vector2 Position { get; set; } // позиция в мире
+        public int Energy { get; private set; }
+        public int Age { get; private set; }
+        public int LastEnergySource { get; private set; }
+        public HashSet<int> EnergySources { get; private set; } = new HashSet<int>();
+
+        public ArtLifePixel()
+        {
+            for (int i = 0; i < Genome.Length; i++)
+            {
+                Genome[i] = (byte)Random.Range(0, 256);
+            }
+            CommandCounter = 0;
+            ActiveGeneIndex = 0;
+            Direction = 0;
+            Position = Vector2Int.zero;
+            Energy = 100; // начальное значение энергии
+            Age = 0;
+            LastEnergySource = 0;
+        }
     }
 }
