95ea18cfbe770450b8c76e0290e8e9c1498b7e35
Andrey Kuzin <141935903+KuzinAndrey0@users.noreply.github.com>
2025-04-13 15:32:11 +0300

    feat: move cell to ECS

---
+++ Assets/_Core/Scripts/Geneses/ArtLife/Components/MineralComponent.cs
@@ -0,0 +1,15 @@
+﻿using Scellecs.Morpeh;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace Geneses.ArtLife.Components
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    public struct MineralComponent : IComponent
+    {
+        public Vector2 Position;
+        public int Value;
+    }
+}
---
+++ Assets/_Core/Scripts/Geneses/ArtLife/Components/OrganicComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Scellecs.Morpeh;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace Geneses.ArtLife.Components
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    public struct OrganicComponent : IComponent
+    {
+        public Vector2 Position;
+        public int NutritionalValue;
+    }
+}
