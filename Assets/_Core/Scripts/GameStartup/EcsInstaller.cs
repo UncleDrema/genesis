cb940df01d5ada4a8a8969f9d26a604e05322f56
Kirill Dremin <missl.wipiece@gmail.com>
2025-04-26 22:34:57 +0300

    chore: replaced extension method

--- Assets/_Core/Scripts/GameStartup/EcsInstaller.cs
+++ Assets/_Core/Scripts/GameStartup/EcsInstaller.cs
@@ -4,7 +4,6 @@ using Genesis.Drawing;
 using Genesis.GameWorld;
 using Scellecs.Morpeh.Addons.Feature;
 using Scellecs.Morpeh.Addons.Feature.Unity;
-using Scellecs.Morpeh.Addons.Unity.VContainer;
 using VContainer;
 
 namespace Genesis.GameStartup
@@ -25,9 +24,9 @@ namespace Genesis.GameStartup
         {
             return new UpdateFeature[]
             {
-                _container.CreateFeature<GameWorldFeature>(),
-                _container.CreateFeature<ArtLifeFeature>(),
-                _container.CreateFeature<DrawingFeature>(),
+                CreateFeature<GameWorldFeature>(_container),
+                CreateFeature<ArtLifeFeature>(_container),
+                CreateFeature<DrawingFeature>(_container)
             };
         }
 
@@ -40,8 +39,16 @@ namespace Genesis.GameStartup
         {
             return new LateUpdateFeature[]
             {
-                _container.CreateFeature<GameWorldLateFeature>()
+                CreateFeature<GameWorldLateFeature>(_container),
             };
+        }
+
+        private static TFeature CreateFeature<TFeature>(IObjectResolver container)
+            where TFeature : BaseFeature
+        {
+            var builder = new RegistrationBuilder(typeof(TFeature), Lifetime.Transient);
+            var registration = builder.Build();
+            return registration.SpawnInstance(container) as TFeature;
         }
     }
 }
